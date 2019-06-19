8628f446edc73ac133dbc24a05d85c0494d89ad2
Mike Peiman <mikepeiman@gmail.com>
2019-06-19 15:01:30 -0700

    Copying components to create a skeleton of the site

--- src/components/HeroHome.vue
+++ src/components/HeroHome.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="hello">
+<div class="hero">
+  <img alt="Vue logo" src="../assets/logo.png" width="20%">
+  <h1 class="title">
     {{ msg }}
-  </div>
+  </h1>
+
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'HeroHome',
   props: {
     msg: String
   }
@@ -14,18 +18,26 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+
+<style lang="scss" scoped>
+
+.hero {
+  background: url(../assets/tulip-bg-1-1000x600.jpg) no-repeat center center fixed;
+}
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Tulip Electronics Inc."/>
+    <HeroHome msg="Hero Home Inc."/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import HeroHome from '@/components/HeroHome.vue'
+import Nav from '@/components/Nav.vue'
+import ProductCard from '@/components/ProductCard.vue'
+import Footer from '@/components/Footer.vue'
+import SignUp from '@/components/SignUp.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    HeroHome
   }
 }
 </script>
+
+<style lang="scss" scoped>
+body {
+  background: black;
+}
+</style>
+
